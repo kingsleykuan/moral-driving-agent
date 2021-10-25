19e12196cdaf5122ee033cb2e03aa95b1a64a770
Zechariah Tan <zx3110@gmail.com>
2021-10-25 14:03:11 +0800

    Add flexibility to DQN network input shape

--- src/learning_network/model.py
+++ src/learning_network/model.py
@@ -94,18 +94,45 @@ class DQNModel(BaseModel):
 
         self.input_size = input_size
         self.dropout_rate = dropout_rate
-        self.input_shape = input_size
         self.num_actions = num_actions
 
-        self.fc_1 = nn.Conv2d(self.input_shape, 32, kernel_size=2)
-        self.batch_norm_1 = nn.BatchNorm2d(32)
-        self.fc_2 = nn.Conv2d(32, 64, kernel_size=2)
-        self.batch_norm_2 = nn.BatchNorm2d(64)
+        modifier = 0
+        layer_1_out = 2 << (4 + modifier)
+        layer_2_out = 2 << (5 + modifier)
+        layer_3_out = 2 << (6 + modifier)
+        fully_connected_out = 2 << (7 + modifier)
 
-        self.feature_layer = nn.Linear(1024, 256)
-        self.action_layer = nn.Linear(256, self.num_actions)
+        self.fc_1 = nn.Conv2d(self.input_shape[2], layer_1_out, kernel_size=2, bias=False)
+        self.batch_norm_1 = nn.BatchNorm2d(layer_1_out)
+        self.fc_2 = nn.Conv2d(layer_1_out, layer_2_out, kernel_size=2, bias=False)
+        self.batch_norm_2 = nn.BatchNorm2d(layer_2_out)
+        self.fc_3 = nn.Conv2d(layer_2_out, layer_3_out, kernel_size=2, bias=False)
+        self.batch_norm_3 = nn.BatchNorm2d(layer_3_out)
+
+        feature_out_size = self.calc_feature_size(input_shape)
+        self.feature_layer = nn.Linear(feature_out_size, fully_connected_out)
+        self.action_layer = nn.Linear(fully_connected_out, self.num_actions)
 
         self.dropout = nn.Dropout(self.dropout_rate)
+
+    def calc_feature_size(self, starting_input_shape):
+        # assuming input shape following (channel, height, width)
+        layers = [module for module in self.modules()]
+        curr_shape = starting_input_shape
+        for layer in layers:
+            if (isinstance(layer, nn.Conv2d)):
+                new_shape_h = int(
+                    ((curr_shape[1] + 2 * layer.padding[0] - layer.dilation[0] * (layer.kernel_size[0] - 1) - 1) /
+                     layer.stride[0]) + 1)
+                new_shape_w = int(
+                    ((curr_shape[2] + 2 * layer.padding[1] - layer.dilation[1] * (layer.kernel_size[1] - 1) - 1) /
+                     layer.stride[1]) + 1)
+                curr_shape = (layer.out_channels, new_shape_h, new_shape_w)
+        numel = 1
+        for dim in curr_shape:
+            numel = numel * dim
+
+        return numel
 
         self.init_parameters()
 
@@ -138,32 +165,25 @@ class DQNModel(BaseModel):
         self.init_parameters()
 
     def forward(self, features, **kwargs):
-        # features = self.batch_norm_input(features)
-        #
-        # features = self.fc_1(features)
-        # features = self.batch_norm_1(features)
-        # features = F.relu(features)
-        # features = self.dropout(features)
-        #
-        # features = self.fc_2(features)
-        # features = self.batch_norm_2(features)
-        # features = F.relu(features)
-        # features = self.dropout(features)
-        #
-        # features = self.fc_3(features)
-        # features = torch.sigmoid(features)
-        # features = torch.squeeze(features, dim=-1)
-
-        features = self.batch_norm_input(features)
-
         features = self.fc_1(features)
-        features = self.relu1(features)
+        features = self.batch_norm_1(features)
+        features = F.relu(features)
+        features = self.dropout(features)
+
         features = self.fc_2(features)
-        features = self.relu2(features)
+        features = self.batch_norm_2(features)
+        features = F.relu(features)
+        features = self.dropout(features)
+
+        features = self.fc_3(features)
+        features = self.batch_norm_3(features)
+        features = F.relu(features)
+        features = self.dropout(features)
 
         features = self.feature_in_layer(features)
-        features = self.relu3(features)
+        features = F.relu(features)
         features = self.action_out_layer(features)
+
         features = torch.sigmoid(features)
         features = torch.squeeze(features, dim=-1)
 
